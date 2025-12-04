7f121c561e93a909c8b0b41fb178e5de4b0c1e2c
Casper Howse <casperhowse@gmail.com>
2025-12-05 12:46:45 +1300

    Export jsx correctly

---
+++ jsx-runtime.ts
@@ -0,0 +1,6 @@
+import { CappaFactory, CappaFragment } from "./mod.ts";
+
+export const jsx      = CappaFactory;
+export const jsxs     = CappaFactory;
+export const jsxdev   = CappaFactory;
+export const Fragment = CappaFragment;
